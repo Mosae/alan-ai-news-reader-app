6627d4706c2ebff64b60d59f25974ada9e0d6ba3
mosaelitsoane@gmail.com <mosaelitsoane@Mosaes-MacBook-Pro.local>
2020-11-28 00:43:10 -0600

    Can display news + images

---
+++ src/components/NewsCards/styles.js
@@ -0,0 +1,9 @@
+import { colors } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+export default makeStyles({
+	container: {
+		padding: '0 5%',
+		width: '100%',
+		margin: 0,
+	},
+});
